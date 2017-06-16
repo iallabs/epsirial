f65ff96667137078c4763b63e8a28656489bef4b
Hilaly <hilalyamine@gmail.com>
2017-06-16 16:27:10 +0200

    change

--- epsirial/site0/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
-models =(models.Product, models.Categorie, models.Shop, models.Offer)
-for m in models:
-    admin.site.register(m)
